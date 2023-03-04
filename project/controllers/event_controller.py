5d96a22970cb0a446c5434329337f8d488780387
sae-eg <sae.eg94@gmail.com>
2023-03-04 11:05:59 +0900

    サムネイル取得＆エラーno表示

--- project/controllers/event_controller.py
+++ project/controllers/event_controller.py
@@ -1,4 +1,4 @@
 from flask import Flask, flash, redirect, render_template, request, session
 
 def event():
-    return render_template("event.html")
+    return render_template("event.html") ##wiki_image=page_data.images[0]
--- project/controllers/summary_controller.py
+++ project/controllers/summary_controller.py
@@ -7,18 +7,24 @@ import wikipedia
 def summary():
     region_name = request.args.get('region')
 
+    if not region_name:
+        flash("regionを入力しよう")
+        return redirect("map.html")
+
     # キーワードで検索
-    wikipedia.set_lang("ja")
-    search_response = wikipedia.search(region_name)
+    else:
+        wikipedia.set_lang("ja")
+        search_response = wikipedia.search(region_name)
 
     #検索結果を表示
     # print('キーワード:'+ str(region_name) + 'での検索結果は' + str(len(search_response)) + '件です。')
     # print(search_response)
     #検索結果のページ内容を表示
-    page_data = wikipedia.page(search_response[0])
+        page_data = wikipedia.page(search_response[0])
     # print(page_data.content) # コンテンツ全て
     # print(page_data.title) # タイトル
     # print(page_data.summary) # 要約
     # print(page_data.html()) # html
+    # print(page_data.images) #全写真url
 
-    return render_template("summary.html", wiki_summary=page_data.summary)
+        return render_template("summary.html", wiki_summary=page_data.summary, wiki_image=page_data.images[0])
