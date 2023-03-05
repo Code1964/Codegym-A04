37df2cc3e15219d2e2a9d6123fae1a118bc974a6
naka-c1024 <ny42800824@gmail.com>
2023-03-05 17:25:07 +0900

    fix: wikipedia-api

--- project/app.py
+++ project/app.py
@@ -19,6 +19,20 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# API keyがセットされていることを確認
+if not os.environ.get("GOOGLE_MAPS_API_KEY"):
+    raise RuntimeError("GOOGLE_MAPS_API_KEY not set")
+
+
+# リクエストを送った後レスポンスがcacheされないように設定している
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 def index():
