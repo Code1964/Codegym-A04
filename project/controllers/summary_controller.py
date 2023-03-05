e98b2139bdf21633d46cbb006981146c77688d40
naka-c1024 <ny42800824@gmail.com>
2023-03-05 17:03:30 +0900

    add: 国データcsvとpandasの追加

--- project/controllers/summary_controller.py
+++ project/controllers/summary_controller.py
@@ -10,15 +10,7 @@ def summary():
     # キーワードで検索
     wikipedia.set_lang("ja")
     search_response = wikipedia.search(region_name)
-
-    #検索結果を表示
-    # print('キーワード:'+ str(region_name) + 'での検索結果は' + str(len(search_response)) + '件です。')
-    # print(search_response)
     #検索結果のページ内容を表示
     page_data = wikipedia.page(search_response[0])
-    # print(page_data.content) # コンテンツ全て
-    # print(page_data.title) # タイトル
-    # print(page_data.summary) # 要約
-    # print(page_data.html()) # html
 
     return render_template("summary.html", wiki_summary=page_data.summary)
