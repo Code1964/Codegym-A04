3b94d1ba1b63771f0d03d2a88101319cbe140213
naka-c1024 <ny42800824@gmail.com>
2023-03-16 16:07:03 +0900

    add: お気に入りのポイントをGoogle mapに表示

--- controllers/favorite_controller.py
+++ controllers/favorite_controller.py
@@ -1,11 +1,31 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+import sqlite3
+
+from helpers import apology
+
+# CREATE TABLE favorite (user_id INT NOT NULL, region TEXT NOT NULL, placeID TEXT UNIQUE);
 
 def favorite():
-    # とりあえずget
-    region_name = request.args.get("region_name")
-    placeID = request.args.get('placeID') # ない場合もある
-    print(region_name)
-    print(placeID)
-    print("========================================")
+    region = request.args.get("region_name")
+    placeID = request.args.get('placeID') # ない場合もある, UNIQUEに設定してある
+    if not region:
+        return apology("must provide region name", 400)
+    # ==== query ====================
+    try:
+        conn = sqlite3.connect("globe.db")
+        cur = conn.cursor()
+        if not placeID:
+            cur.execute("INSERT INTO favorite (user_id, region, placeID) VALUES(?, ?, ?)", (session["user_id"], region, 'null'))
+        else:
+            cur.execute("INSERT INTO favorite (user_id, region, placeID) VALUES(?, ?, ?)", (session["user_id"], region, placeID))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except:
+        return apology("Database operation failed", 400)
+    # ================================
 
-    return redirect(url_for('flashcard'))
+    if not placeID:
+        flash("placeID not found")
+        return redirect(url_for('index'))
+    return redirect(url_for('map'))
